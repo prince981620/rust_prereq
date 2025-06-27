21b58e3a71c42090b7dfa8096770ebee2ebe20a5
prince981620 <prince981620@gmail.com>
2025-06-28 01:48:10 +0530

    adds rust prereq

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,73 @@ pub fn add(left: u64, right: u64) -> u64 {
 
 #[cfg(test)]
 mod tests {
-    use solana_sdk::{signature::{Keypair, Signer}, pubkey::Pubkey};
-    use super::*;
+    use bs58;
+    use solana_client::{nonblocking::rpc_client, rpc_client::RpcClient};
+    use solana_program::{hash::hash, pubkey::Pubkey, system_instruction::transfer};
+    use solana_sdk::{
+        instruction::{AccountMeta, Instruction}, message::Message, signature::{read_keypair_file, Keypair, Signer}, system_program, transaction::{self, Transaction}
+    };
+    use std::io::{self, BufRead};
+    use std::str::FromStr;
+    const RPC_URL: &str = "https://api.devnet.solana.com";
+
+    #[test]
+    fn enroll() {
+        let signer = read_keypair_file("turbin3-wallet.json").expect("Couldn't find wallet file");
+        let mint = Keypair::new();
+        let turbin3_prereq_program =
+            Pubkey::from_str("TRBZyQHB3m68FGeVsqTK39Wm4xejadjVhP5MAZaKWDM").unwrap();
+        let collection = Pubkey::from_str("5ebsp5RChCGK7ssRZMVMufgVZhd2kFbNaotcZ5UvytN2").unwrap();
+        let mpl_core_program =
+            Pubkey::from_str("CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d").unwrap();
+        let system_program = system_program::id();
+
+        let signer_pubkey = signer.pubkey();
+
+        print!("here is the userpubkey {}", signer_pubkey);
+        let seeds = &[b"prereqs", signer_pubkey.as_ref()];
+        let (prereq_pda, _bump) = Pubkey::find_program_address(seeds, &turbin3_prereq_program);
+
+        let seeds = &[b"collection", collection.as_ref()];
+        let (authority, _bump) = Pubkey::find_program_address(seeds, &turbin3_prereq_program);
+
+        let data = vec![77, 124, 82, 163, 21, 133, 181, 206];
+
+        let accounts = vec![
+            AccountMeta::new(signer.pubkey(), true),      // user signer
+            AccountMeta::new(prereq_pda, false),          // PDA account
+            AccountMeta::new(mint.pubkey(), true),        // mint keypair
+            AccountMeta::new(collection, false),          // collection
+            AccountMeta::new_readonly(authority, false), // authority (PDA)
+            AccountMeta::new_readonly(mpl_core_program, false), // mpl core program
+            AccountMeta::new_readonly(system_program, false), // system program
+        ];
+
+        let blockhash = client
+            .get_latest_blockhash()
+            .expect("Failed to get blockhash!");
+
+        let instruction = Instruction {
+            program_id: turbin3_prereq_program,
+            accounts,
+            data,
+        };
+
+        let transaction = Transaction::new_signed_with_payer(
+            &[instruction],
+            Some(&signer.pubkey()),
+            &[&signer, &mint],
+            blockhash,
+        );
+
+        let signature = client
+            .send_and_confirm_transaction(&transaction)
+            .expect("Failed to send transaction");
+        println!(
+            "Success! Check out your TX here:\nhttps://explorer.solana.com/tx/{}/?cluster=devnet",
+            signature
+        );
+    }
 
     #[test]
     fn keygen() {
@@ -18,13 +83,67 @@ mod tests {
 
     #[test]
     fn airdrop() {
-        
+        let keypair = read_keypair_file("dev-wallet.json").expect("Coudn't find wallet file");
+        let client = RpcClient::new(RPC_URL);
+        match client.request_airdrop(&keypair.pubkey(), 2_000_000_000u64) {
+            Ok(s) => {
+                println! {"Success! check your Tx here:"};
+                println! {"https://explorer.solana.com/tx/{}?cluster=devnet",s.to_string()};
+            }
+            Err(e) => println!("Oops, something went wrong :{}", e.to_string()),
+        };
     }
 
     #[test]
     fn transfer_sol() {
-        
+        let keypair = read_keypair_file("dev-wallet.json").expect("Coudn't find wallet file");
+        let pubkey = keypair.pubkey();
+        let message_bytes = b"I verify my solana Keypair";
+        let sig = keypair.sign_message(message_bytes);
+        let sig_hashed = hash(sig.as_ref());
+
+        match sig.verify(&pubkey.to_bytes(), &sig_hashed.to_bytes()) {
+            true => println!("Signature verified"),
+            false => println!("Signature not verified"),
+        }
+
+        let to_pubkey = Pubkey::from_str("7MBcR9GQs94CWwL2SwzgSuhTa8guzb1dAVTDPCpnFzr9").unwrap();
+
+        let rpc_client = RpcClient::new(RPC_URL);
+
+        let recent_blockhash = rpc_client
+            .get_latest_blockhash()
+            .expect("Falied to get recent blockhash");
+
+        let transaction = Transaction::new_signed_with_payer(
+            &[transfer(&keypair.pubkey(), &to_pubkey, 1_000_000)],
+            Some(&keypair.pubkey()),
+            &vec![&keypair],
+            recent_blockhash,
+        );
+
+        let signature = rpc_client
+            .send_and_confirm_transaction(&transaction)
+            .expect("Failed to send transaction");
+
+        println!(
+            "Success! Check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet",
+            signature
+        );
     }
+
+
+        #[test]
+    fn bs58_to_wallet() {
+        println!("input your private key as base58:");
+        let stdin = io::stdin();
+        let base58 = stdin.lock().lines().next().unwrap().unwrap();
+        println!("Your wallet file is:");
+        let wallet = bs58::decode(base58).into_vec().unwrap();
+        println!("{:?}", wallet);
+    }
+
+
 
 
 }
